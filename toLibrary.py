27ea9e4b129f5311a90b9a7f4571455b719698ac
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-08-16 19:55:03 +0500

    updates

--- toLibrary.py
+++ toLibrary.py
@@ -2,23 +2,39 @@ import os, pathlib, shutil
 import sys, platform
 from setuptools import Extension, setup
 
-SOURCE_DIR = "path/to/folder"
-source_files = []
+SOURCE_DIRS = []
+SOURCE_FILES = []
 
-for root, dirs, files in os.walk(SOURCE_DIR):
-    for file in files:
-        if (not file.endswith((".py", ".pyx"))) or file.startswith("__init__"):
-            continue
-        name = file[: file.rindex(".")]
-        source_files.append((name, os.path.join(root, file)))
+
+for dir_or_file in sys.argv[1:]:
+    if os.path.isdir(dir_or_file):
+        SOURCE_DIRS.append(os.path.abspath(dir_or_file))
+    elif os.path.isfile(dir_or_file):
+        SOURCE_FILES.append(os.path.abspath(dir_or_file))
+
+
+for source_dir in SOURCE_DIRS:
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            if (not file.endswith((".py", ".pyx"))) or file.startswith("__init__"):
+                continue
+            name = file[: file.rindex(".")]
+            SOURCE_FILES.append((name, os.path.join(root, file)))
+
+
+if not SOURCE_FILES:
+    sys.exit("No available source found.")
+
 
 sys.argv[1:] = ["build_ext", "--inplace"]
-ext_modules = [Extension(name, [source]) for name, source in source_files]
+ext_modules = [Extension(name, [source]) for name, source in SOURCE_FILES]
 setup(ext_modules=ext_modules)
+
 
 base_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 build_dir = os.path.join(base_dir, "build")
 shutil.rmtree(build_dir, ignore_errors=True)
+
 
 extension = ".pyd" if platform.system().lower() == "windows" else ".so"
 dynamic_libraries = [
@@ -27,7 +43,8 @@ dynamic_libraries = [
     if (os.path.isfile(file) and file.endswith(extension))
 ]
 
-for name, source in source_files:
+
+for name, source in SOURCE_FILES:
     c_file = source[: source.rindex(".")] + ".c"
     pathlib.Path(c_file).unlink(missing_ok=True)
 
