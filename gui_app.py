4028149d561e6cede2c381ac549ac48db84412a0
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-08-18 09:20:29 +0500

    updates

--- gui_app.py
+++ gui_app.py
@@ -33,18 +33,12 @@ def get_source_files(sources, exclude_dirs):
         ):
             source_dirs.add(dir_or_file.resolve())
 
-        elif (
-            dir_or_file.is_file()
-            and dir_or_file.suffix == ".py"
-            and dir_or_file.stem not in ["__init__", "main"]
-        ):
+        elif dir_or_file.is_file() and dir_or_file.suffix == ".py":
             source_files.add((dir_or_file.stem, str(dir_or_file.resolve())))
 
     for source_dir in source_dirs:
         for file in source_dir.rglob("*.py"):
-            if not any(
-                ex in file.parts for ex in exclude_dirs
-            ) and dir_or_file.stem not in ["__init__", "main"]:
+            if not any(ex in file.parts for ex in exclude_dirs):
                 source_files.add((file.stem, str(file.resolve())))
 
     return sorted(source_files)
@@ -53,21 +47,29 @@ def get_source_files(sources, exclude_dirs):
 def generate_library(name, source, out_dir):
     logging.info(f"Generating library for {name}")
     try:
-        shutil.rmtree(out_dir, ignore_errors=True)
+        copy_to_dir = os.path.join(os.path.dirname(source) + ".library")
+        os.makedirs(copy_to_dir, exist_ok=True)
 
+        if name in {"__init__", "main"}:
+            copy_from = source
+            copy_to = os.path.join(copy_to_dir, name) + ".py"
+
+            pathlib.Path(copy_to).unlink(missing_ok=True)
+            shutil.copyfile(copy_from, copy_to)
+
+            return
+
+        shutil.rmtree(out_dir, ignore_errors=True)
         stubgen.main([source])
         setup(ext_modules=[Extension(name, [source])])
 
-        move_to_dir = os.path.join(os.path.dirname(source) + ".library")
-        os.makedirs(move_to_dir, exist_ok=True)
-
         for root, dirs, files in os.walk(out_dir):
             for file in files:
-                move_from = os.path.join(root, file)
-                move_to = os.path.join(move_to_dir, file)
+                copy_from = os.path.join(root, file)
+                copy_to = os.path.join(copy_to_dir, file)
 
-                pathlib.Path(move_to).unlink(missing_ok=True)
-                shutil.move(move_from, move_to)
+                pathlib.Path(copy_to).unlink(missing_ok=True)
+                shutil.copyfile(copy_from, copy_to)
 
     except Exception as e:
         logging.error(f"Failed to generate library for {name}: {e}")
